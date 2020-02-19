99d5d50804efa6893d8b2d7472c6cb94493c945d
wrublik93 <wrublik.93@mail.ru>
2020-02-20 00:14:08 +0300

    feat: implement three functions: max, min and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || !array.length){
+        return 0;
+    } else {
+        array.sort(function(a,b) {
+            return a - b;
+        });
+        return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || !array.length){
+        return 0;
+    } else {
+        array.sort(function(a,b){
+            return a - b;
+        });
+        return array[array.length - 1];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || !array.length){
+        return 0;
+    } else {
+        let filter = 0;
+        array.forEach(function(item){
+            filter += item;
+        });
+        return filter / array.length;
+    }
 }
